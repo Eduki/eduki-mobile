9bffbed96587869f7716ae6ff1987cc556b30518
RafaelV <rafvertido@gmail.com>
2013-05-05 00:31:26 -0700

    added methods for lesson urls

--- eduki/src/com/huskysoft/eduki/data/UrlConstants.java
+++ eduki/src/com/huskysoft/eduki/data/UrlConstants.java
@@ -2,10 +2,18 @@
 package com.huskysoft.eduki.data;
 
 /**
- * @author Cody Thomas Class UrlConstants maintains a collection of variables
- *         representing appropriate api urls
+ * @author Cody Thomas, Rafael Vertido Class UrlConstants maintains a 
+ * 		   collection of variables representing appropriate api urls
  */
 public final class UrlConstants {
     public static final String BASE = "http://eduki.herokuapp.com/api/";
     public static final String COURSES = BASE + "courses";
+    
+    public static String getAllLessonsURL(int course_id) {
+    	return COURSES + "/" + course_id + "/lessons";
+    }
+    
+    public static String getSingleLessonURL(int course_id, int lesson_id) {
+    	return getAllLessonsURL(course_id) + "/" + lesson_id;
+    }
 }
