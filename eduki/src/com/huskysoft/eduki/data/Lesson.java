4e149edf98b87cb6a3a858626f3921cd87de59f1
RafaelV <rafvertido@gmail.com>
2013-05-05 00:30:35 -0700

    Matched Lesson with API fields. Updated LessonQuery to use UrlConstants methods

--- eduki/src/com/huskysoft/eduki/data/Lesson.java
+++ eduki/src/com/huskysoft/eduki/data/Lesson.java
@@ -1,23 +1,45 @@
 package com.huskysoft.eduki.data;
 
+/**
+ * @author Rafael Vertido Lesson represents a lesson with an id, title, and body,
+ * 		   and also its corresponding course's id
+ */
+
 public class Lesson {
     private int id;
+    private int course_id;
     private String title;
-    private String content;
+    private String body;
     
+    public Lesson(int id, String title, int course_id, String body) {
+    	this.id = id;
+    	this.title = title;
+    	this.course_id = course_id;
+    	this.body = body;
+    }
+    
+    /** @return the string representation of this lesson */
     public String toString() {
         return getTitle();
     }
     
+    /** @return the id of the lesson */
     public int getId() {
-        return id;
+    	return id;
     }
     
+    /** @return the id of the course this lesson is associated with */
+    public int getCourseId() {
+        return course_id;
+    }
+    
+    /** @return the title of the lesson */
     public String getTitle() {
-        return title;
+    	return title;
     }
     
-    public String getContent() {
-    	return content;
+    /** @return the content of the lesson */
+    public String getBody() {
+    	return body;
     }
 }
--- eduki/src/com/huskysoft/eduki/data/LessonQuery.java
+++ eduki/src/com/huskysoft/eduki/data/LessonQuery.java
@@ -1,7 +1,5 @@
 package com.huskysoft.eduki.data;
 
-import static com.huskysoft.eduki.data.UrlConstants.COURSES;
-
 import java.lang.reflect.Type;
 import java.util.List;
 
@@ -9,25 +7,41 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.huskysoft.eduki.TaskComplete;
 
+/**
+ * @author Rafael Vertido LessonQuery will make queries related to lessons to the api
+ */
 public class LessonQuery {
-    public static void getAllLessons(TaskComplete callback, String course_id) {
-    	String lessonsInCourseUrl = COURSES + "/" + course_id + "/lessons";
-        new ConnectionTask(callback).execute(lessonsInCourseUrl);
+    /**
+     * Will get a list of all lessons, returned to the callback as a string
+     * 
+     * @param callback The callback for when the request is complete
+     */
+    public static void getAllLessons(TaskComplete callback, int course_id) {
+    	String getLessonsURL = UrlConstants.getAllLessonsURL(course_id);
+        new ConnectionTask(callback).execute(getLessonsURL);
     }
     
-    public static void getSpecificLesson(TaskComplete callback, String course_id, String lesson_id) {
-    	String specificLessonUrl = COURSES + "/" + course_id + "/lessons/" + lesson_id;
+    /**
+     * Will get a specific lesson, returned to the callback as a string
+     * 
+     * @param callback The callback for when the request is complete
+     */
+    public static void getSpecificLesson(TaskComplete callback, int course_id, int lesson_id) {
+    	String specificLessonUrl = UrlConstants.getSingleLessonURL(course_id, lesson_id);
     	new ConnectionTask(callback).execute(specificLessonUrl);
     }
     
+    /**
+     * Will parse the string and return a list of lessons represented in the
+     * string.
+     * 
+     * @param data The json string to be parsed
+     * @return A list of lessons represented in the string
+     */
     public static List<Lesson> parseLessonsList(String data) {
         Gson gson = new Gson();
         Type collectionType = new TypeToken<List<Lesson>>(){}.getType();
         List<Lesson> lessons = gson.fromJson(data, collectionType);
         return lessons;
     }
-
-	public static String parseLessonContent(String data) {
-		return data;
-	}
 }
