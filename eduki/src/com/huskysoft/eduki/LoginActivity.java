48fc5fc250d852d508d4778c3afb00e35d0b1868
Cody Thomas <codythomas2013@gmail.com>
2013-06-01 15:29:35 -0700

    Login page completed, removed unused classes/xml

--- eduki/src/com/huskysoft/eduki/LoginActivity.java
+++ eduki/src/com/huskysoft/eduki/LoginActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.huskysoft.eduki.data.UserQuery;
@@ -33,6 +34,8 @@ public class LoginActivity extends Activity implements TaskComplete {
         String user = userBox.getText().toString();
         TextView passBox = (TextView) findViewById(R.id.passwordBox);
         String pass = passBox.getText().toString();
+        ((ProgressBar) findViewById(R.id.progressBar1)).setVisibility(View.VISIBLE);
+        ((TextView) findViewById(R.id.loginError)).setVisibility(View.INVISIBLE);
         UserQuery.attemptLogin(user, pass, this, 0);
     }
 
@@ -40,11 +43,14 @@ public class LoginActivity extends Activity implements TaskComplete {
     public void taskComplete(String data, int id) {
         // TODO If data doesn't give me an error, store the authtoken in shared
         // preferences
+        ((ProgressBar) findViewById(R.id.progressBar1)).setVisibility(View.INVISIBLE);
         boolean error = false;
         if (!error) {
             Intent returnIntent = new Intent();
             setResult(RESULT_OK, returnIntent);
             finish();
+        } else {
+            ((TextView) findViewById(R.id.loginError)).setVisibility(View.VISIBLE);
         }
     }
 }
